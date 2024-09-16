ee8c618bfd688e36fcebe1c927634c411398f9b6
tsomielvis@gmail.com <tsomielvis@gmail.com>
2024-09-16 17:18:38 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 34, "M", "Blue", "aggressive", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 14, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 36, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 23, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 4, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 11, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 19, "M", "black", "aggressive", 0);
